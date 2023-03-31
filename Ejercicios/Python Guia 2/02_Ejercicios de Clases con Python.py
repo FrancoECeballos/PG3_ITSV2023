b1f7576a20f688afe6177348f649ed49e1d290ef
Franco <franco.ceballos2005@gmail.com>
2023-03-31 11:46:08 -0300

    Guía 2 Ejercicio 3

--- Ejercicios/Python Guia 2/02_Ejercicios de Clases con Python.py
+++ Ejercicios/Python Guia 2/02_Ejercicios de Clases con Python.py
@@ -27,4 +27,32 @@ alumno1 = Alumno()
 alumno2 = Alumno()
 
 alumno1.checkNota(input("Introduce el nombre del alumno 1: "), int(input("Introduce la nota del alumno 1: ")))
-alumno2.checkNota(input("Introduce el nombre del alumno 2: "), int(input("Introduce la nota del alumno 2: ")))
+alumno2.checkNota(input("Introduce el nombre del alumno 2: "), int(input("Introduce la nota del alumno 2: ")))#/
+
+print("Ejercício 3: \n")
+
+class Triangulo():
+    def __init__(self, lado1, lado2, lado3):
+        self.lado1 = lado1
+        self.lado2 = lado2
+        self.lado3 = lado3
+    def ladoMayor(self):
+        if self.lado1 > self.lado2 and self.lado1 > self.lado3:
+            print("\nEl lado mayor es", self.lado1, "\n")
+        elif self.lado2 > self.lado1 and self.lado2 > self.lado3:
+            print("\nEl lado mayor es", self.lado2, "\n")
+        elif self.lado3 > self.lado1 and self.lado3 > self.lado2:
+            print("\nEl lado mayor es", self.lado3, "\n")
+        else:
+            print("\nNo hay un solo lado que sea mayor que el resto\n")
+    def tipoTriangulo(self):
+        if self.lado1 == self.lado2 and self.lado1 == self.lado3:
+            print("Es un triángulo equilátero\n")
+        elif self.lado1 == self.lado2 or self.lado1 == self.lado3 or self.lado2 == self.lado3:
+            print("Es un triángulo isósceles\n")
+        else:
+            print("Es un triángulo escaleno\n")
+
+triangulo1 = Triangulo(int(input("Introduce el lado 1: ")), int(input("Introduce el lado 2: ")), int(input("Introduce el lado 3: ")))
+triangulo1.ladoMayor()
+triangulo1.tipoTriangulo()
