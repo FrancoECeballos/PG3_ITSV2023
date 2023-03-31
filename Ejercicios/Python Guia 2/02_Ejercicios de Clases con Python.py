f0f321fda5c09f44dc9fd5370a3eb767aba62a7d
Franco <franco.ceballos2005@gmail.com>
2023-03-31 11:16:15 -0300

    Guía 2 Ejercicio 1

--- Ejercicios/Python Guia 2/02_Ejercicios de Clases con Python.py
+++ Ejercicios/Python Guia 2/02_Ejercicios de Clases con Python.py
@@ -1,1 +1,14 @@
-print("hola")
+print("Ejercício 1: \n")
+
+class Persona():
+    def setNombre(self, nombre):
+        self.nombre = nombre
+        print("Nombre definido\n")
+    def printNombre(self):
+        print("Mi nombre es ", self.nombre, "\n")
+
+p1 = Persona
+p1.setNombre(p1, input("Introduce tu nombre: "))
+p1.printNombre(p1)
+
+
