add02868cb8f96f7d3f7ef2c6b2ace4e33cf8e04
Franco <franco.ceballos2005@gmail.com>
2023-04-14 11:38:11 -0300

    Guía 3 Ejercicio 2

--- Ejercicios/Python Guia 3/3_Manejo_de_exceptiones_con_python.py
+++ Ejercicios/Python Guia 3/3_Manejo_de_exceptiones_con_python.py
@@ -9,6 +9,23 @@ while True:
     except ValueError:
         print("\nHubo un error en el ingreso de datos. El dato ingresado no es un múmero")
     finally:
-        continuar=input("¿Desea continuar? Si desea detener el programa, ingrese 'N': ")
+        continuar=input("¿Desea continuar? Si desea detener el programa, ingrese 'N': \n")
+        if continuar=="N" or continuar=="n":
+            break
+
+print("\nEjercicio N°2: \n")
+
+while True:
+    try:
+        num1=int(input("Ingrese un numero: "))
+        num2=int(input("Ingrese otro numero: "))
+        division= num1 / num2
+        print("\nEl resultado es: ", division)
+    except ValueError:
+        print("\nHubo un error en el ingreso de datos. El dato ingresado no es un múmero")
+    except ZeroDivisionError:
+        print("\nHubo un error en el ingreso de datos. No se puede dividir por 0")
+    finally:
+        continuar=input("¿Desea continuar? Si desea detener el programa, ingrese 'N': \n")
         if continuar=="N" or continuar=="n":
             break
